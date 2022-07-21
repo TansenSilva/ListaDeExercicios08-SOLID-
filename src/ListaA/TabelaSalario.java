5e2e6f540873dd19d710a3ed004ecfeba047f461
TansenSilva <tansensilva@gmail.com>
2022-07-21 18:11:48 -0300

    Envio exercício finalizado. Implementação OCP

---
+++ src/ListaA/TabelaSalario.java
@@ -0,0 +1,10 @@
+package ListaA;
+
+public interface TabelaSalario {
+
+    double faixaAjusteSalarial();
+
+    //criei a interface para que não precise alterar a classe Financeiro pra cada nova faixa de reajuste de salario.
+
+
+}
---
+++ src/ListaA/TabelaSalarioFaixa2.java
@@ -0,0 +1,10 @@
+package ListaA;
+
+public class TabelaSalarioFaixa2 implements TabelaSalario{
+
+    @Override
+    public double faixaAjusteSalarial() {
+        return 0.05;
+    }
+
+}
